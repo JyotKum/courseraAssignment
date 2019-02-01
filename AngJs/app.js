6996e54698ccf69e4578d700df9f1e5cf20d5633
JyotKum <40595381+JyotKum@users.noreply.github.com>
2019-02-01 16:53:18 +0530

    Add files via upload

--- AngJs/app.js
+++ AngJs/app.js
@@ -1,11 +1,28 @@
 (function(){
 	'use strict';
-	angular.module('diApp',[])
-	.controller('diAppController',diAppController);
-	diAppController.$inject=['$scope','$filter']
-	function diAppController($scope,$filter){
-		$scope.name="Jyoti";
-		$scope.totalValue=101;
+	angular.module('LunchCheck',[])
+	.controller('LunchCheckController',LunchCheckController);
+	LunchCheckController.$inject=['$scope']
+	function LunchCheckController($scope){
+		$scope.enterMenu='';
+		$scope.showMessage='';
+		$scope.showMenu=function (){
+			var arr=[];
+			var menuItem=$scope.enterMenu;
+			if(menuItem.length==0){
+				$scope.showMessage="Please enter data first";
+			}else{
+				 arr=menuItem.split(",");
+			 var itemLength=arr.length;
+			 if(itemLength<4){
+			$scope.showMessage="Enjoy!";
+			 }else{
+				 $scope.showMessage="Too much!";
+			 }
+				
+			}
+			
+		}
 	};
 	
 	
